9dc04f6a1bd4cbfe2e3089bfd2fc072b68476b4e
Shane Alcock <shane@alcock.co.nz>
2023-04-30 18:12:03 -0400

    Allow multiple S-ARIMA detector threads

--- watchtower/sentry/filters/SArimaChocolatine.py
+++ watchtower/sentry/filters/SArimaChocolatine.py
@@ -2,6 +2,7 @@ import logging, time
 from libchocolatine.libchocolatine import ChocolatineDetector
 from libchocolatine.asyncfetcher import AsyncHistoryFetcher
 from .. import SentryModule
+from iterators import TimeoutIterator
 
 IODAAPI="https://api.ioda.inetintel.cc.gatech.edu/v2/signals/raw"
 
@@ -25,6 +26,7 @@ add_cfg_schema = {
                 "port": {"type": "integer"},
             },
         },
+        "detectors": {"type": "integer", "exclusiveMinimum": 0},
         "maxarma": {"type": "integer", "exclusiveMinimum": 0},
         "minpredict": {"type": "integer", "inclusiveMinimum": 0},
     },
@@ -46,6 +48,8 @@ class SArimaChocolatine(SentryModule.SentryModule):
         self.kafkaconf['bootstrap-model'] = "capri.cc.gatech.edu:9092"
         self.kafkaconf['group'] = "ioda-watchtower-chocolatine-%s" % (self.name)
 
+        self.numdetectors = config.get("detectors", 8)
+
         if "kafkaconf" in config:
             if 'modellerTopic' in config['kafkaconf']:
                 self.kafkaconf['modellertopic'] = config['kafkaconf']['modellerTopic']
@@ -56,45 +60,67 @@ class SArimaChocolatine(SentryModule.SentryModule):
 
         ctx['method'] = "sarima" # for AlertKafka
 
+        self.detectors = []
+        self.fetchers = []
 
-        self.detector = ChocolatineDetector(self.name, IODAAPI, self.kafkaconf,
-                self.dbconf, self.maxarma)
-        self.fetcher = AsyncHistoryFetcher(IODAAPI, self.detector.histRequest,
-                self.detector.histReply)
+    def startDetectorPool(self):
 
-        self.fetcher.start()
-        self.detector.start()
+        for i in range(0, self.numdetectors):
+            name = "%s-%3d" % (self.name, i)
+            self.detectors.append(ChocolatineDetector(name, IODAAPI,
+                    self.kafkaconf, self.dbconf, self.maxarma))
+            self.fetchers.append(AsyncHistoryFetcher(IODAAPI,
+                    self.detectors[i].histRequest,
+                    self.detectors[i].histReply))
+
+            self.fetchers[i].start()
+            self.detectors[i].start()
+
+    def haltDetectorPool(self):
+        for d in self.detectors:
+            d.halt()
+        for f in self.fetchers:
+            f.halt()
+        self.detectors = []
+        self.fetchers = []
 
     def run(self):
         logger.debug("SArimaChocolatine.run()")
-        for entry in self.gen():
-            key, value, t = entry
-            if value is None:
-                continue
-            self.detector.queueLiveData(key, t, value)
+        self.startDetectorPool()
 
-            while True:
-                ev = self.detector.getLiveDataResult(False)
-                if ev is None:
-                    break
 
-                if ev[2] is not None:
-                    # Ignore any "events" where the normal time series
-                    # is below an accepted minimum value (e.g. regions
-                    # where the normal metric value is close to zero).
-                    if int(ev[2]['predicted']) < self.minpredict:
-                        val = 1.0
-                    elif ev[2]['alertable'] and ev[2]['threshold'] > 0:
-                        val = ev[2]['observed'] / ev[2]['threshold']
-                    else:
-                        val = 1.0
+        it = TimeoutIterator(self.gen(), timeout=0.5)
+        for entry in it:
+            if entry != it.get_sentinel():
+                key, value, t = entry
+                if value is None:
+                    continue
+                detid = hash(key) % self.numdetectors
 
-                    actual = int(ev[2]['observed'])
-                    pred = int(ev[2]['predicted'])
-                    yield((ev[0], (val, actual, pred), ev[1]))
+                self.detectors[detid].queueLiveData(key, t, value)
 
-                if ev[0] == key and ev[1] == t:
-                    break
+            for d in self.detectors:
+                while True:
+                    ev = d.getLiveDataResult(False)
+                    if ev is None:
+                        break
 
-        self.detector.halt()
-        self.fetcher.halt()
+                    if ev[2] is not None:
+                        # Ignore any "events" where the normal time series
+                        # is below an accepted minimum value (e.g. regions
+                        # where the normal metric value is close to zero).
+                        if int(ev[2]['predicted']) < self.minpredict:
+                            val = 1.0
+                        elif ev[2]['alertable'] and ev[2]['threshold'] > 0:
+                            val = ev[2]['observed'] / ev[2]['threshold']
+                        else:
+                            val = 1.0
+
+                        actual = int(ev[2]['observed'])
+                        pred = int(ev[2]['predicted'])
+                        yield((ev[0], (val, actual, pred), ev[1]))
+
+                        if ev[0] == key and ev[1] == t:
+                            break
+
+        self.haltDetectorPool()
