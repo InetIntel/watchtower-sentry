1791920dc1c0e93b8faf39058d92e1c7fca8fea3
Shane Alcock <shane@alcock.co.nz>
2023-07-12 03:08:51 +0000

    AlertKafka: add option to suppress "normal" alerts for minduration
    This can be used to ensure that a return to "normal" is not
    transient before declaring that an ongoing event is over, much
    like we already require multiple alertable observations before we
    trigger an alert being written into kafka.
    
    Note: this change is optional and off by default, so existing
    watchtower configurations will continue to behave as they did
    before (but we may want to consider adopting this change for
    them as well!)

--- watchtower/sentry/sinks/AlertKafka.py
+++ watchtower/sentry/sinks/AlertKafka.py
@@ -8,6 +8,8 @@ Configuration parameters ('*' indicates required parameter):
     minduraton: (number) Only generate alerts for events at least this long.
     brokers*: (string) Comma-separated list of kafka brokers.
     topic*: (string) Kafka topic prefix.
+    disable: (boolean) Disable writing alert objects to kafka, i.e. for debug.
+    waitnormal: (boolean) Apply minduration when detecting return to normal.
 
     At least one of {min} or {max} is required.
 
@@ -35,6 +37,7 @@ add_cfg_schema = {
         "brokers":     {"type": "string"},
         "topic": {"type": "string"},
         "disable":     {"type": "boolean"}, # for debugging
+        "waitnormal":  {"type": "boolean"}, 
     },
     "required": ["fqid", "name", "brokers", "topic"],
     "oneOf": [{"required": ["min"]}, {"required": ["max"]}]
@@ -57,6 +60,7 @@ class AlertKafka(SentryModule.Sink):
         self.max = config.get('max', None)
         self.minduration = config.get('minduration', None)
         self.disable = config.get('disable', False)
+        self.waitnormal = config.get('waitnormal', False)
         self.condition_label = [
             "< %r" % self.min,   # -1
             "normal",            # 0
@@ -65,6 +69,7 @@ class AlertKafka(SentryModule.Sink):
 
         self.alert_status = dict()  # alert_status[key] = [0,1,-1]
         self.alert_state = dict()  # alert_state[key] = (time, value, actual, predicted)
+        self.normal_state = dict()  # alert_state[key] = (time, value, actual, predicted)
         kp_cfg = {
             'bootstrap.servers': self.brokers,
         }
@@ -157,17 +162,7 @@ class AlertKafka(SentryModule.Sink):
                                         actual, predicted)
                 elif alert_status == STATUS_NORMAL:
                     # back to normal
-                    if key not in self.alert_state:
-                        # back to normal, but we have a minduration set, so we
-                        # would have tracked state for this event. given that
-                        # there is no state, it means the event was long enough
-                        # to trigger an alert, so we need to trigger the
-                        # normal event
-                        logger.info("Creating normal alert for %s at %d" %
-                                    (key, t))
-                        self._produce_alert(alert_status, t, key, value,
-                                            actual, predicted)
-                    else:
+                    if key in self.alert_state:
                         # back to normal, and we have a minduration, so given
                         # that there is state being tracked, we haven't yet
                         # reached the minduration, so the outage must have been
@@ -182,11 +177,38 @@ class AlertKafka(SentryModule.Sink):
                                            "(init_t: %d, t: %d, minduration: %d)"
                                            % (key, init_t, t, self.minduration))
                         del self.alert_state[key]
+                    elif self.waitnormal and key not in self.normal_state:
+                        # possibly returning to normal after being in an alert
+                        # state, but we need a few more observations to be sure
+                        self.normal_state[key] = (t, value, actual, predicted)
+                        logger.info("Suppressing normal alert for %s" % key)
+                    elif not self.waitnormal:
+                        logger.info("Creating normal alert for %s at %d" %
+                                    (key, t))
+                        self._produce_alert(alert_status, t, key, value,
+                                            actual, predicted)
+
+
                 else:
-                    # we have a minduration, and this is an "outage" event,
-                    # start tracking state
-                    self.alert_state[key] = (t, value, actual, predicted)
-                    logger.info("Suppressing alert for %s" % key)
+                    if self.waitnormal and key in self.normal_state:
+                        # we had started to see a return to normal, but now
+                        # we've fallen back into an alert state so let's just
+                        # stick with alert for now
+                        (init_t, init_v, init_a, init_p) = self.normal_state[key]
+                        logger.info("Discarding suppressed normal alert for '%s' "
+                                    "(init_t: %d, t: %d, minduration: %d)"
+                                    % (key, init_t, t, self.minduration))
+                        if (t - init_t) > self.minduration:
+                            logger.warning("Discarding suppressed normal alert for "
+                                           "'%s' that exceeds minduration "
+                                           "(init_t: %d, t: %d, minduration: %d)"
+                                           % (key, init_t, t, self.minduration))
+                        del self.normal_state[key]
+                    else:
+                        # we have a minduration, and this is an "outage" event,
+                        # start tracking state
+                        self.alert_state[key] = (t, value, actual, predicted)
+                        logger.info("Suppressing alert for %s" % key)
             elif alert_status != STATUS_NORMAL:
                 # continuation of the event (but not continuation of normal)
                 if key in self.alert_state:
@@ -203,10 +225,25 @@ class AlertKafka(SentryModule.Sink):
                         del self.alert_state[key]
                     else:
                         logger.info("Continuing to suppress alert for %s "
-                                    "(duration: %d)" % (key, t - init_t))
+                                "(duration: %d)" % (key, t - init_t))
+
             else:
-                # continuation of normal, who cares
-                pass
+                # continuation of normal
+                if self.waitnormal and key in self.normal_state:
+                    # we haven't triggered the normal alert yet, check the
+                    # duration in case we need to alert
+                    (init_t, init_v, init_a, init_p) = self.normal_state[key]
+                    if (init_t + self.minduration) <= t:
+                        logger.info("Suppressed normal alert for '%s' passed minduration "
+                                    "(init_t: %d, t: %d, minduration: %d)" %
+                                    (key, init_t, t, self.minduration))
+                        self._produce_alert(alert_status, init_t, key, init_v,
+                                            init_a, init_p)
+                        del self.normal_state[key]
+
+                    else:
+                        logger.info("Continuing to suppress normal alert for %s "
+                                "(duration: %d)" % (key, t - init_t))
 
         self.kproducer.flush()
         logger.debug("AlertKafka.run() done")
